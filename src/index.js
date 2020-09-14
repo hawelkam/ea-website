52dec7935914225410f96c933d98b537fd1fc420
hawelkam <m.hawelka@gmail.com>
2020-09-14 16:27:51 +0200

    Project initialization + basic translation service

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import common_pl from "./locale/pl/common.json";
+import common_en from "./locale/en/common.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "pl",
+  resources: {
+    pl: {
+      common: common_pl,
+    },
+    en: {
+      common: common_en,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
