610944d4ce0259ff15e716d09c18970231d8e194
Michał Hawełka <m.hawelka@gmail.com>
2020-10-04 10:21:30 +0200

    TopBar centered

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -8,6 +8,7 @@ import {
   NavbarBrand,
   Nav,
   Button,
+  Col
 } from "reactstrap";
 import eaLogo from "../../assets/images/efektywny-altruizm-logo.svg";
 import { Link } from "react-router-dom";
@@ -29,36 +30,47 @@ const TopBar = () => {
   return (
     <div>
       <Navbar expand="lg" className="">
-        <NavbarBrand href="/">
-          <object type="image/svg+xml" data={eaLogo} className="logo">
-            EA Logo
-          </object>
-        </NavbarBrand>
-        <NavbarToggler className="main-nav__toggler" onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar className="justify-content-end">
-          <Nav navbar>
-            <Link to="/movement" className="main-nav__item">
-              {t("main_nav.about_movement")}
-            </Link>
-            <Link to="/foundation" className="main-nav__item">
-              {t("main_nav.about_foundation")}
-            </Link>
-            <Link to="/join-us" className="main-nav__item">
-              {t("main_nav.join_us")}
-            </Link>
-            <Link to="/blog" className="main-nav__item">
-              {t("main_nav.blog")}
-            </Link>
-            <div onClick={switchLanguage} className="main-nav__item">
-              <img src={lang === "pl" ? ukFlag : plFlag} alt={lang === "pl" ? "ukFlag" : "plFlag"}/>
+        <Col xs={2}>
+          <NavbarBrand href="/">
+            <object type="image/svg+xml" data={eaLogo} className="logo">
+              EA Logo
+            </object>
+          </NavbarBrand>
+          <NavbarToggler className="main-nav__toggler" onClick={toggle} />
+
+        </Col>
+        <Col xs={10} className="d-flex justify-content-end">
+          <Collapse isOpen={isOpen} navbar className="justify-content-end">
+            <Col xs={10}>
+            <Nav navbar className="justify-content-center">
+              <Link to="/movement" className="main-nav__item">
+                {t("main_nav.about_movement")}
+              </Link>
+              <Link to="/foundation" className="main-nav__item">
+                {t("main_nav.about_foundation")}
+              </Link>
+              <Link to="/join-us" className="main-nav__item">
+                {t("main_nav.join_us")}
+              </Link>
+              <Link to="/blog" className="main-nav__item">
+                {t("main_nav.blog")}
+              </Link>
+              <div onClick={switchLanguage} className="main-nav__item">
+                <img src={lang === "pl" ? ukFlag : plFlag} alt={lang === "pl" ? "ukFlag" : "plFlag"}/>
+              </div>
+            </Nav>
+              </Col>
+              <Col xs={2}>
+              <div className="main-nav__button--wrapper">
+              <Button className="main-nav__button">
+                {t("main_nav.contact")}
+              </Button>
             </div>
-          </Nav>
-          <div className="main-nav__button--wrapper">
-            <Button className="main-nav__button">
-              {t("main_nav.contact")}
-            </Button>
-          </div>
-        </Collapse>
+              </Col>
+            
+          </Collapse>
+        </Col>
+        
       </Navbar>
     </div>
   );
